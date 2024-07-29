fe81b83a6a57c895d3148cb6ebe6e35299cd257a
Zohanubis <zohanubis2509@gmail.com>
2024-07-29 21:11:40 +0700

    fix navbar

--- src/customer/components/Product/Product.jsx
+++ src/customer/components/Product/Product.jsx
@@ -177,7 +177,6 @@ export default function Product() {
                             </button>
                         </div>
                     </div>
-
                     <section aria-labelledby="products-heading" className="pb-24 pt-6">
                         <h2 id="products-heading" className="sr-only">
                             Products
@@ -189,7 +188,7 @@ export default function Product() {
                                 {filters.map((section) => (
                                     <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
                                         <h3 className="-my-3 flow-root">
-                                            <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                            <Disclosure.Button className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
                                                 <span className="font-medium text-gray-900">{section.name}</span>
                                                 <span className="ml-6 flex items-center">
                                                     <PlusIcon
@@ -201,9 +200,9 @@ export default function Product() {
                                                         className="h-5 w-5 [.group:not([data-open])_&]:hidden"
                                                     />
                                                 </span>
-                                            </DisclosureButton>
+                                            </Disclosure.Button>
                                         </h3>
-                                        <DisclosurePanel className="pt-6">
+                                        <Disclosure.Panel className="pt-6">
                                             <div className="space-y-4">
                                                 {section.options.map((option, optionIdx) => (
                                                     <div key={option.value} className="flex items-center">
@@ -224,61 +223,62 @@ export default function Product() {
                                                     </div>
                                                 ))}
                                             </div>
-                                        </DisclosurePanel>
+                                        </Disclosure.Panel>
                                     </Disclosure>
                                 ))}
                                 {singleFilters.map((section) => (
                                     <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
-                                        <h3 className="-my-3 flow-root">
-                                            <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                                                {/* <span className="font-medium"></span> */}
-                                                <FormLabel
-                                                    className="text-gray-900"
-                                                    id="demo-radio-buttons-group-label"
-                                                >
-                                                    {section.name}
-                                                </FormLabel>
-                                                <span className="ml-6 flex items-center">
-                                                    <PlusIcon
-                                                        aria-hidden="true"
-                                                        className="h-5 w-5 group-data-[open]:hidden"
-                                                    />
-                                                    <MinusIcon
-                                                        aria-hidden="true"
-                                                        className="h-5 w-5 [.group:not([data-open])_&]:hidden"
-                                                    />
-                                                </span>
-                                            </DisclosureButton>
-                                        </h3>
-                                        <DisclosurePanel className="pt-6">
-                                            <div className="space-y-4">
-                                                {section.options.map((option, optionIdx) => (
-                                                    <FormControl>
-                                                        <RadioGroup
-                                                            aria-labelledby="demo-radio-buttons-group-label"
-                                                            defaultValue="female"
-                                                            name="radio-buttons-group"
-                                                        >
-                                                            <FormControlLabel
-                                                                value="female"
-                                                                control={<Radio />}
-                                                                label="Female"
-                                                            />
-                                                            <FormControlLabel
-                                                                value="male"
-                                                                control={<Radio />}
-                                                                label="Male"
-                                                            />
-                                                            <FormControlLabel
-                                                                value="other"
-                                                                control={<Radio />}
-                                                                label="Other"
-                                                            />
-                                                        </RadioGroup>
-                                                    </FormControl>
-                                                ))}
-                                            </div>
-                                        </DisclosurePanel>
+                                        {({ open }) => (
+                                            <FormControl>
+                                                <h3 className="-my-3 flow-root">
+                                                    <Disclosure.Button className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
+                                                        <span className="font-medium text-gray-900">
+                                                            {section.name}
+                                                        </span>
+                                                        <span className="ml-6 flex items-center">
+                                                            {open ? (
+                                                                <MinusIcon
+                                                                    aria-hidden="true"
+                                                                    className="h-5 w-5 [.group:not([data-open])_&]:hidden"
+                                                                />
+                                                            ) : (
+                                                                <PlusIcon
+                                                                    aria-hidden="true"
+                                                                    className="h-5 w-5 group-data-[open]:hidden"
+                                                                />
+                                                            )}
+                                                        </span>
+                                                    </Disclosure.Button>
+                                                </h3>
+                                                <Disclosure.Panel className="pt-6">
+                                                    <div className="space-y-4">
+                                                        {section.options.map((option, optionIdx) => (
+                                                            <RadioGroup
+                                                                aria-labelledby="demo-radio-buttons-group-label"
+                                                                defaultValue="female"
+                                                                name="radio-buttons-group"
+                                                            >
+                                                                <FormControlLabel
+                                                                    value="female"
+                                                                    control={<Radio />}
+                                                                    label="Female"
+                                                                />
+                                                                <FormControlLabel
+                                                                    value="male"
+                                                                    control={<Radio />}
+                                                                    label="Male"
+                                                                />
+                                                                <FormControlLabel
+                                                                    value="other"
+                                                                    control={<Radio />}
+                                                                    label="Other"
+                                                                />
+                                                            </RadioGroup>
+                                                        ))}
+                                                    </div>
+                                                </Disclosure.Panel>
+                                            </FormControl>
+                                        )}
                                     </Disclosure>
                                 ))}
                             </form>
