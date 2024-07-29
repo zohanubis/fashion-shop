ebceb9649b6d458cfc367d4e44130087b015fe4f
Zohanubis <zohanubis2509@gmail.com>
2024-07-30 01:10:42 +0700

    add section product details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 
 function App() {
     return (
@@ -11,7 +12,9 @@ function App() {
             <Navigation />
             <div>
                 {/* {<HomePage />} */}
-                <Product />
+                {/* <Product />
+                 */}
+                <ProductDetails />
             </div>
             <div>
                 <Footer />
