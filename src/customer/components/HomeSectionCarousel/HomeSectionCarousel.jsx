4290d96758f630792324f35d48ade88ed7940ce3
Zohanubis <zohanubis2509@gmail.com>
2024-07-29 15:59:15 +0700

    add data card

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { ButtonBase } from '@mui/material';
+import { mens_kurta } from '../../../Data/mens_kurta';
 
 const HomeSectionCarousel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    //Giới hạn số lượng product sẽ hiển thị
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 4 },
+        1024: { items: 5.5 },
     };
 
-    const items = [1, 1, 1, 1, 1].map((item, index) => <HomeSectionCard key={index} />);
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+
+    const syncActiveIndex = ({ item }) => setActiveIndex(item);
+    const items = mens_kurta.slice(0, 10).map((item) => <HomeSectionCard product={item} />);
 
     return (
-        <div className="px-4 lg:px-8 border border-black">
+        <div className="border">
             <div className="relative p-5">
                 <AliceCarousel
                     items={items}
@@ -22,31 +29,43 @@ const HomeSectionCarousel = () => {
                     infinite
                     responsive={responsive}
                     disableDotsControls
+                    onSlideChange={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <ButtonBase
-                    variant="contained"
-                    className="z-50 bg-white"
-                    sx={{
-                        position: 'absolute',
-                        top: '8rem',
-                        right: '0rem',
-                        transform: 'translateX(50%) rotate(90deg)',
-                        bgcolor: 'white',
-                    }}
-                    aria-label="next"
-                >
-                    <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
-                </ButtonBase>
+                {activeIndex !== items.length - 5 && (
+                    <ButtonBase
+                        variant="contained"
+                        className="z-50 bg-white"
+                        onClick={slideNext}
+                        sx={{
+                            position: 'absolute',
+                            top: '8rem',
+                            right: '0rem',
+                            transform: 'translateX(50%) rotate(90deg)',
+                            bgcolor: 'white',
+                            '&:hover': {
+                                bgcolor: 'grey.300',
+                            },
+                        }}
+                        aria-label="next"
+                    >
+                        <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                    </ButtonBase>
+                )}
 
                 <ButtonBase
                     variant="contained"
                     className="z-50 bg-white"
+                    onClick={slidePrev}
                     sx={{
                         position: 'absolute',
                         top: '8rem',
                         left: '0rem',
-                        transform: 'translateX(50%) rotate(-90deg)',
+                        transform: 'translateX(-50%) rotate(-90deg)',
                         bgcolor: 'white',
+                        '&:hover': {
+                            bgcolor: 'grey.300',
+                        },
                     }}
                     aria-label="next"
                 >
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <div>
             <MainCrosel />
-            <div className="space-y-10 py-20 flex flex-col justify-center">
+            <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
                 <HomeSectionCarousel />
                 <HomeSectionCarousel />
                 <HomeSectionCarousel />
